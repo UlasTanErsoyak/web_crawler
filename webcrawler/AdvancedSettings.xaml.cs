0dbb51550b78bde46e696b7f4388054f58ca811f
UlasTanErsoyak <ulastanersoyak@hotmail.com>
2023-07-19 21:48:00 +0300

    initial commit

---
+++ webcrawler/AdvancedSettings.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace webcrawler
+{
+    /// <summary>
+    /// Interaction logic for AdvancedSettings.xaml
+    /// </summary>
+    public partial class AdvancedSettings : Window
+    {
+        public AdvancedSettings(string settings_path)
+        {
+            InitializeComponent();
+        }
+        private void Settings_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure you want to close the Advanced Settings window?", "Closing", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.No)
+            {
+                e.Cancel = true;
+
+            }
+        }
+
+        private void back_to_default_settings_button_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO
+        }
+    }
+}
